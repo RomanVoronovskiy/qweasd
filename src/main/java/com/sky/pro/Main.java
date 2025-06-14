f010ad33bab8e0c269701393883ad1f6377bc107
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-06-14 12:44:36 +0300

    add dz

--- src/main/java/com/sky/pro/Main.java
+++ src/main/java/com/sky/pro/Main.java
@@ -2,6 +2,10 @@ package com.sky.pro;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world222");
+        System.out.println("Hello world222");
+        System.out.println("Hello world222");
+        System.out.println("Hello world222");
+        System.out.println("Hello world222");
     }
 }
