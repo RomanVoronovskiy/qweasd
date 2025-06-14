33e76488c38521da0cf828521ae1d1547a4abc37
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-06-14 13:02:34 +0300

    Update Main.java

--- src/main/java/com/sky/pro/Main.java
+++ src/main/java/com/sky/pro/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world222");
         System.out.println("Hello world222ьпоронарева");
-        System.out.println("Hello world222");
+        System.out.println("Hello world222     ");
         System.out.println("Hello world222");
         System.out.println("Hello world222");
     }
