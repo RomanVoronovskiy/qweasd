310a5e0653a6406b05999f6c359293c0a6fee456
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-07-05 11:08:47 +0300

    Revert "add hello ru (#4)"
    This reverts commit bc67ea23e22412a2b438a76515e993115413ba0e.

--- src/main/java/com/sky/pro/Main.java
+++ src/main/java/com/sky/pro/Main.java
@@ -2,7 +2,7 @@ package com.sky.pro;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет мир !");
+        System.out.println("Hello world!");
 
 
     }
